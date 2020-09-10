4075a81fa737529996cfd5e885c472255e3e7708
faith <faith@marascientific.org>
2020-09-10 19:08:33 +0300

    changes

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,9 +1,8 @@
 const weatherForecast = "weather-forecast-v1";
 const assets = [
-    "/",
-    "/index.html",
-    "/styles.css",
-    "/index.js",
+    "index.html",
+    "styles.css",
+    "index.js",
 ];
 
 self.addEventListener("install", installEvent => {
